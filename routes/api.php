2b98fcce3fd0db1530d556409f39cab27c9510f1
kksingh22 <ihavenotanyemail@gmail.com>
2023-08-10 21:15:00 +0530

    third commit

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\API\UserController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,4 +16,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+Route::post('login',[UserController::class,'loginUser']);
+
+
+Route::group(['middleware' => 'auth:sanctum'],function(){
+    Route::get('user',[UserController::class,'userDetails']);
+    Route::get('logout',[UserController::class,'logout']);
 });
