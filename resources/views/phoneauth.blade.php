ceaf34ad30b43ed5f254021ecf955a79f85b55fb
kksingh22 <ihavenotanyemail@gmail.com>
2023-08-10 23:05:42 +0530

    fifth phone coloumn

--- resources/views/phoneauth.blade.php
+++ resources/views/phoneauth.blade.php
@@ -33,6 +33,13 @@
 
     <script>
         var firebaseConfig = {
+            apiKey: "API_KEY",
+            authDomain: "PROJECT_ID.firebaseapp.com",
+            databaseURL: "https://PROJECT_ID.firebaseio.com",
+            projectId: "PROJECT_ID",
+            storageBucket: "PROJECT_ID.appspot.com",
+            messagingSenderId: "SENDER_ID",
+            appId: "APP_ID"
             
         };
         firebase.initializeApp(firebaseConfig);
